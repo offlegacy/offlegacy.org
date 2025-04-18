3745a28e50c0b9c8a255825a91f75c90f8b6cdae
gwansikk <gwansik.kim@gwansik.dev>
2025-04-18 07:50:29 -0400

    feat: add sonner toaster

--- apps/offlegacy.org/app/layout.tsx
+++ apps/offlegacy.org/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#0A0A0A] text-[#FAFAFA]`}>{children}</body>
+      <body className={`${inter.className} bg-[#0A0A0A] text-[#FAFAFA]`}>
+        {children}
+        <Toaster position="top-right" closeButton />
+      </body>
     </html>
   )
 }
