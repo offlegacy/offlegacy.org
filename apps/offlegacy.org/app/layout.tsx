d1e0a1e437dc56f1380f1950586a8f5ea5fd1212
Gwansik Kim <gwansik.kim@gwansik.dev>
2025-05-22 20:45:44 +0900

    feat: posthog 연동 (#3)
    * feat: posthog 연동
    
    * fix: remove hardcoded userId from Track.Provider

--- apps/offlegacy.org/app/layout.tsx
+++ apps/offlegacy.org/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Toaster } from 'sonner'
 import './globals.css'
+import Provider from './provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#0A0A0A] text-[#FAFAFA]`}>
-        {children}
-        <Toaster position="top-right" closeButton />
+        <Provider>
+          {children}
+          <Toaster position="top-right" closeButton />
+        </Provider>
       </body>
     </html>
   )
