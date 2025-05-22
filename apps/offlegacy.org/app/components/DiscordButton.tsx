7dd8d1fb83f92aa28ccdfea382ab99c916335097
gwansikk <gwansik.kim@gwansik.dev>
2025-05-22 20:24:22 +0900

    feat: posthog 연동

--- apps/offlegacy.org/app/components/DiscordButton.tsx
+++ apps/offlegacy.org/app/components/DiscordButton.tsx
@@ -3,6 +3,7 @@
 import { FaDiscord } from 'react-icons/fa'
 import Button from './Button'
 import { toast } from 'sonner'
+import Track from '../utils/event-tracker'
 
 export default function DiscordButton() {
   const handleClick = () => {
@@ -10,11 +11,13 @@ export default function DiscordButton() {
   }
 
   return (
-    <Button className="bg-[#546BBD] hover:bg-[#546BBD]/90 shadow-md shadow-[#546BBD]/50" onClick={handleClick}>
-      <div className="flex items-center gap-2">
-        <FaDiscord size={24} />
-        <span className="text-sm font-semibold whitespace-nowrap">JOIN OFFLEGACY DISCORD</span>
-      </div>
-    </Button>
+    <Track.Click schema="click_button" params={{ event: 'discord_button' }}>
+      <Button className="bg-[#546BBD] hover:bg-[#546BBD]/90 shadow-md shadow-[#546BBD]/50" onClick={handleClick}>
+        <div className="flex items-center gap-2">
+          <FaDiscord size={24} />
+          <span className="text-sm font-semibold whitespace-nowrap">JOIN OFFLEGACY DISCORD</span>
+        </div>
+      </Button>
+    </Track.Click>
   )
 }
---
+++ apps/offlegacy.org/app/utils/event-tracker.ts
@@ -0,0 +1,33 @@
+import { createTracker } from '@offlegacy/event-tracker'
+import posthog from 'posthog-js'
+import { z } from 'zod'
+
+interface Context {
+  userId: string
+}
+
+interface Params {
+  event: string
+}
+
+const schemas = {
+  page_view: z.object({
+    title: z.string(),
+  }),
+  click_button: z.object({
+    event: z.string(),
+  }),
+}
+
+const [Track] = createTracker<Context, Params, typeof schemas>({
+  DOMEvents: {
+    onClick: (params: Params) => {
+      posthog.capture(params.event)
+    },
+  },
+  schema: {
+    schemas,
+  },
+})
+
+export default Track
